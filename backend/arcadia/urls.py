6fdc056da3df59f0f002fb991de5e312988669dd
Auston Pawell <nepgyah@gmail.com>
2024-10-13 23:37:00 +0900

    [ADD] Basic 404

--- backend/arcadia/urls.py
+++ backend/arcadia/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-
+from django.urls import path, include, re_path
+from public_site.views import not_found
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("account/", include("users.urls")),
-    path('', include('public_site.urls'))
+    path('', include('public_site.urls')),
+
+    re_path(r'^.*$', not_found),
 ]
--- backend/public_site/urls.py
+++ backend/public_site/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from . import views
+from django.conf.urls import handler404
 
 app_name = 'public_site'
+
+handler404 = views.not_found
 
 urlpatterns = [
     path('', views.homepage, name='homepage'),
