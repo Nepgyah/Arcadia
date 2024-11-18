85cec88dc54082760eaa724759512eeb880a1136
Auston Pawell <nepgyah@gmail.com>
2024-11-18 20:50:05 +0900

    [ADD] Base user model

--- backend/arcadia/settings.py
+++ backend/arcadia/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'corsheaders',
-    'users'
+    'user'
 ]
 
 MIDDLEWARE = [
@@ -136,3 +136,5 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'user.User'
