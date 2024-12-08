3adaafae31b94f8702949a8a7bd82d7dee50e6ed
Auston Pawell <nepgyah@gmail.com>
2024-12-08 20:05:08 +0900

    [FIX] Topnav
    Added home log and fixed styling

--- frontend/app/components/navigation/topNav.tsx
+++ frontend/app/components/navigation/topNav.tsx
@@ -2,16 +2,24 @@ import { Avatar, IconButton } from "@mui/material";
 import React from "react";
 
 import AppsIcon from '@mui/icons-material/Apps';
+import Link from "next/link";
 
 const TopNav = () => {
     return (
-        <div className="top-nav">
-            <IconButton >
-                <Avatar />
-            </IconButton>
-            <IconButton >
-                <AppsIcon />
-            </IconButton>
+        <div className="top-bar">
+            <div className="logo">
+                <Link href="/">
+                    <img src="../../static/images/logo_white.svg" />
+                </Link>
+            </div>
+            <div className="navigation">
+                <IconButton >
+                    <AppsIcon />
+                </IconButton>
+                <IconButton >
+                    <Avatar />
+                </IconButton>
+            </div>
         </div>
     )
 }
