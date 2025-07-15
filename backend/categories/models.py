6ccacbeaafdc6a2739958cef08ab4aaf80a7b011
Auston <nepgyah@gmail.com>
2025-07-15 22:00:53 +0900

    (feat) Add ratings and genre models

--- backend/categories/models.py
+++ backend/categories/models.py
@@ -1,3 +1,12 @@
+# Collection of models/choices that will be used multiple times
 from django.db import models
 
-# Create your models here.
+class RatingChoices(models.IntegerChoices):
+    G = 0, 'G'
+    E = 1, 'G'
+    PG = 2, 'PG'
+    PG13 = 4, 'PG-13'
+
+class Genre(models.Model):
+    name = models.CharField(max_length=255)
+
