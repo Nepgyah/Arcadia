21eaab44646d8cc6466a09b99213cb58c66a2b6a
Auston <nepgyah@gmail.com>
2025-07-22 19:26:54 +0900

    chore: create shared model migrations

--- backend/shared/admin.py
+++ backend/shared/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Talent, Character, Company
 
 # Register your models here.
+admin.site.register(Talent)
+admin.site.register(Character)
+admin.site.register(Company)
