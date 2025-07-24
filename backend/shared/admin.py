3f7a6f1251cdfaf488e3d721f7edfaed9827bbbf
Auston <nepgyah@gmail.com>
2025-07-24 19:22:14 +0900

    fix: add genre model to admin

--- backend/shared/admin.py
+++ backend/shared/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
-from .models import Talent, Character, Company
+from .models import Talent, Character, Company, Genre
 
 # Register your models here.
 admin.site.register(Talent)
 admin.site.register(Company)
+admin.site.register(Genre)
 
 @admin.register(Character)
 class CharacterAdmin(admin.ModelAdmin):
