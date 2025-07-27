15b54fdfd28ca30f8430c189bd5eeb042231c286
Auston <nepgyah@gmail.com>
2025-07-27 20:53:08 +0900

    feat: create case study page

---
+++ frontend/src/app/(public)/resource/case-study/page.tsx
@@ -0,0 +1,15 @@
+export default function PageCaseStudy() {
+    return (
+        <div id="page-resource-case-study">
+            
+            <section id="hero" className="hero">
+                <div className="hero__wrapper hero__wrapper--two-column">
+                     <div className='hero__text'>
+                        <h1>Case Studies</h1>
+                        <p className="main-text">Case studies text</p>
+                    </div>
+                </div>
+            </section>
+        </div>
+    )
+}
