fea96ebbf28d68af06726da21d450ad71fd1a94e
Auston <nepgyah@gmail.com>
2025-08-06 23:04:25 +0900

    feat: add PSU + Cooler models

--- backend/kumitateru/models.py
+++ backend/kumitateru/models.py
@@ -56,9 +56,6 @@ class CPU(Part):
     max_temp=models.IntegerField(null=True, blank=True, help_text='Celcius')
     is_unlocked=models.BooleanField(help_text='Overclockable')
     has_graphics=models.BooleanField()
-
-    def __str__(self):
-        return self.name
     
 class MemoryType(models.Model):
     """
@@ -75,9 +72,6 @@ class RAM(Part):
     capacity=models.IntegerField(null=True, blank=True)
     modules=models.IntegerField(null=False, blank=False, default=1, help_text='Number of sticks/modules')
     speed=models.IntegerField(null=True, blank=True, help_text='MHz')
-
-    def __str__(self):
-        return self.name
 
 class Chipset(models.Model):
     name = models.CharField(max_length=100)
@@ -101,9 +95,6 @@ class Motherboard(Part):
     max_memory=models.IntegerField()
     m2_slots=models.IntegerField()
     sata_slots=models.IntegerField()
-
-    def __str__(self):
-        return self.name
 
 class GPUMemoryType(models.Model):
     name=models.CharField(max_length=20, null=False, blank=False)
@@ -129,6 +120,43 @@ class GPU(Part):
     tdp=models.IntegerField(null=True, blank=True, help_text="Watts")
     suggested_psu=models.IntegerField(null=True, blank=True, help_text="Watts")
     slot_width=models.IntegerField(choices=SlotWidth.choices, default=SlotWidth.DUAL)
+    
+class PSU(Part):
 
-    def __str__(self):
-        return self.name
+    class Efficiency(models.IntegerChoices):
+        BRONZE = 1, 'Bronze'
+        SILVER = 2, 'Silver'
+        GOLD = 3, 'Gold'
+        PLATINUM = 4, 'Platinum'
+
+    class PSUType(models.IntegerChoices):
+        ATX = 1, 'ATX'
+        SFX = 2, 'SFX'
+
+    wattage = models.IntegerField()
+    efficiency = models.IntegerField(choices=Efficiency.choices, default=Efficiency.BRONZE, help_text='Defaults to bronze')
+    type = models.IntegerField(choices=PSUType.choices, default=PSUType.ATX, help_text='Defaults to ATX')
+    is_modular = models.BooleanField()
+    has_zero_rpm = models.BooleanField()
+    weight = models.FloatField(help_text='kg')
+    length = models.IntegerField()
+    width = models.IntegerField()
+    height = models.IntegerField()
+    connector_8_pin_count = models.IntegerField(default=0)
+    connector_6_2_pin_count = models.IntegerField(default=0)
+    connector_6_pin_count = models.IntegerField(default=0)
+    connector_4_molex_count = models.IntegerField(default=0)
+    connector_sata_count = models.IntegerField(default=0)
+
+class CPUCooler(Part):
+    
+    class CoolerType(models.IntegerChoices):
+        AIO = 0, "Water - AIO"
+        SINGLE = 1, 'Air - Single Tower'
+        DUAL = 2, 'Air - Dual Tower'
+    
+    supported_sockets = models.ManyToManyField(Socket, related_name='compatible_coolers')
+    type = models.IntegerField(choices=CoolerType.choices, default=CoolerType.SINGLE)
+    height = models.IntegerField(null=True, blank=True, help_text='mm')
+    has_fans = models.BooleanField()
+    radiator_size = models.IntegerField(null=True, blank=True, help_text='mm')
