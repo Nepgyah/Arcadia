30b14cf2637cda67ed28b2e3a61d803476fdab8b
Auston <nepgyah@gmail.com>
2025-08-26 21:11:34 +0900

    feat: add franchise model

--- backend/shared/models.py
+++ backend/shared/models.py
@@ -1,12 +1,29 @@
 from django.db import models
 from django.utils.text import slugify
 
+class Franchise(models.Model):
+    
+    name = models.CharField(max_length=255)
+    slug = models.SlugField(unique=True, blank=True)
+    description = models.TextField(default='A summary will be written later.')
+    
+    def __str__(self):
+        return self.name
+    
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
+
 class Media(models.Model):
+    """
+    Used as the base for Miru, Yomu, and Asobu
+    """
+
     title = models.CharField(max_length=255)
     score = models.FloatField(default=0.0)
     users = models.IntegerField(default=0)
     slug = models.SlugField(unique=True, blank=True)
-    summary=models.TextField(default='A synopsis will be written later')
+    summary=models.TextField(default='A synopsis will be written later.')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
