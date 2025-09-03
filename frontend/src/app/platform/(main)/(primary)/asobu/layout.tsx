f153d2a0c3cd0b4b768ab60cdd12220fa68fe27a
Auston <nepgyah@gmail.com>
2025-09-03 20:32:45 +0900

    feat: add asobu home

---
+++ frontend/src/app/platform/(main)/(primary)/asobu/layout.tsx
@@ -0,0 +1,8 @@
+// Layout to apply asobu color scheming
+export default function AsobuLayout({children} : {children: React.ReactNode}) {
+    return (
+        <div id="asobu-layout">
+            {children}
+        </div>
+    )
+}
