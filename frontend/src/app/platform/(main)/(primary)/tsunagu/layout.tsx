99878539c79f1240ce3d24342edfb2901cfb2d95
Auston <nepgyah@gmail.com>
2025-09-08 13:33:53 +0900

    feat: add tsunagu base homepage

---
+++ frontend/src/app/platform/(main)/(primary)/tsunagu/layout.tsx
@@ -0,0 +1,8 @@
+// Layout to apply asobu color scheming
+export default function TsunaguLayout({children} : {children: React.ReactNode}) {
+    return (
+        <div id="tsuangu-layout">
+            {children}
+        </div>
+    )
+}
--- frontend/src/app/platform/(main)/(primary)/tsunagu/page.tsx
+++ frontend/src/app/platform/(main)/(primary)/tsunagu/page.tsx
@@ -1,5 +1,49 @@
+'use client';
+
+import { Breadcrumbs, Typography } from "@mui/material";
+import { apiGET } from "@/util/api/api";
+import { useEffect, useState } from "react";
+import React from "react";
+import { useUser } from "@/util/wrappers/userContext";
+
 export default function TsunaguHome() {
+    const { user } = useUser()
+    
+    const [trendingPosts, setTrendingPosts] = useState([])
+    const [userCommunities, setUserCommunities] = useState([])
+
+    useEffect(() => {
+        apiGET<any>('tsunagu/home/')
+        .then((res) => {
+            setTrendingPosts(res.trending_posts);
+        })
+    }, [])
+    
     return (
-        <h1>Tsunagu Home</h1>
+        <React.Fragment>
+            <Breadcrumbs>
+                <Typography>Asobu</Typography>
+                <Typography>Home</Typography>
+            </Breadcrumbs>
+            <div id="page-asobu-home"  className="page-content page-content--two-col page-content--reversed">
+                <div className="page-content__left-column divider divider--vertical padding-right--lg">
+                    <div id="latest">
+                        <h2>Trending</h2>
+                        <div className="layout-grid-1">
+                            Posts
+                        </div>
+                    </div>
+                </div>
+                <div className="page-content__right-column">
+                    <h2 className="app-font--miru border-bottom">Your Communities</h2>
+                    {
+                        user ?
+                            'Your Communities'
+                        :
+                            'Login to view your communities'
+                    }
+                </div>
+            </div>
+        </React.Fragment>
     )
 }
