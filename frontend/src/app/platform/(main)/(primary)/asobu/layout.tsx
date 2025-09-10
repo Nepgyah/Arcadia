3506fab61f4d5bc841a24a6850403d26118008ce
Auston <nepgyah@gmail.com>
2025-09-10 22:04:30 +0900

    refactor: migrate/rename layout app styles

--- frontend/src/app/platform/(main)/(primary)/asobu/layout.tsx
+++ frontend/src/app/platform/(main)/(primary)/asobu/layout.tsx
@@ -1,7 +1,9 @@
 // Layout to apply asobu color scheming
+import '@/styles/platform/apps/asobu.scss';
+
 export default function AsobuLayout({children} : {children: React.ReactNode}) {
     return (
-        <div id="asobu-layout">
+        <div id="asobu-app">
             {children}
         </div>
     )
--- frontend/src/app/platform/(main)/(secondary)/kumitateru/layout.tsx
+++ frontend/src/app/platform/(main)/(secondary)/kumitateru/layout.tsx
@@ -1,7 +1,9 @@
 // Layout to apply kumitateru color scheming
+import '@/styles/platform/apps/kumitateru.scss';
+
 export default function KumitateruLayout({children} : {children: React.ReactNode}) {
     return (
-        <div id="kumitateru-layout">
+        <div id="kumitateru-app">
             {children}
         </div>
     )
