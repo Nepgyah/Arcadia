e09a5b7a3d2fea54dffac7efda2169a20b1f2ac3
Auston <nepgyah@gmail.com>
2025-11-05 20:15:59 +0900

    feat: convert anime detail page to ssr

--- platform/src/app/(main)/(primary)/miru/anime/[id]/[anime_slug]/page.tsx
+++ platform/src/app/(main)/(primary)/miru/anime/[id]/[anime_slug]/page.tsx
@@ -1,9 +1,6 @@
-'use client';
-
-import { useParams } from "next/navigation";
+export const revalidate = 3600;
 
 import React from "react";
-import { useEffect, useState } from "react";
 import { Breadcrumbs, Typography } from "@mui/material";
 import { GraphQL } from "@/util/api/api";
 
@@ -19,79 +16,72 @@ import { Anime, AnimeTheme } from "@/types/miru";
 import '@/styles/layout/_media-detail.scss';
 import KikuCard from "@/app/(main)/(secondary)/kiku/kikuCard";
 
-export default function AnimeDetails() {
-    const params = useParams();
-    const [anime, setAnime] = useState<Anime>()
-
-    useEffect(() => {
-        const query = 
-        `
-            query {
-                animeById(id: ${params.id}) {
+export default async function AnimeDetails({ params } : {params: { id: string}}) {
+    const query = 
+    `
+        query {
+            animeById(id: ${params.id}) {
+                id,
+                title,
+                franchise {
                     id,
-                    title,
-                    franchise {
-                        id,
-                        name,
-                        socials
-                    },
-                    score,
-                    users,
-                    slug,
-                    summary,
-                    season,
-                    status,
-                    characters {
-                    character {
+                    name,
+                    socials
+                },
+                score,
+                users,
+                slug,
+                summary,
+                season,
+                status,
+                characters {
+                character {
+                    id,
+                    firstName,
+                    lastName,
+                    playedBy {
                         id,
                         firstName,
-                        lastName,
-                        playedBy {
-                            id,
-                            firstName,
-                            lastName
-                        }
-                    },
-                    role
-                    },
-                    previousAnime {
-                        relationType
-                        fromAnime {
-                            id,
-                            slug,
-                            title
-                        }
-                    },
-                    nextAnime {
-                        relationType
-                        toAnime {
-                            id,
-                            slug,
-                            title
-                        }
-                    },
-                    type,
-                    studio {
-                    name
-                    },
-                    rating,
-                    airingStartDate,
-                    airingEndDate,
-                    themes
-                }
+                        lastName
+                    }
+                },
+                role
+                },
+                previousAnime {
+                    relationType
+                    fromAnime {
+                        id,
+                        slug,
+                        title
+                    }
+                },
+                nextAnime {
+                    relationType
+                    toAnime {
+                        id,
+                        slug,
+                        title
+                    }
+                },
+                type,
+                studio {
+                name
+                },
+                rating,
+                airingStartDate,
+                airingEndDate,
+                themes
             }
-        `
-        GraphQL<any>(query)
-        .then((res) => {
-            setAnime(res.data.animeById)
-        })
-    }, [])
+        }
+    `
+    const res = await GraphQL<any>(query);
+    const anime = res.data.animeById
 
     return (
         <React.Fragment>
             <Breadcrumbs>
                 <Typography>Anime</Typography>
-                <Typography>{anime?.title}</Typography>
+                <Typography>{anime.title}</Typography>
             </Breadcrumbs>
             <div id="page-media-detail" className="page-content">
                 <div className="grid grid--feature-combo">
@@ -100,7 +90,7 @@ export default function AnimeDetails() {
                         description={anime ? anime.summary : 'Summary'}
                         score={anime ? anime.score : 0.0}
                         app="miru"
-                        image={`/storage/miru/${anime?.id}.jpg`}
+                        image={`/storage/miru/${anime.id}.jpg`}
                      />
                     <div id="latest-episode" className="border-radius-md bg-platform-dark box-shadow p-a-lg">
                         <WIP />
@@ -118,31 +108,31 @@ export default function AnimeDetails() {
                             <ArcHeader title="Socials" />
                             <div id="socials-container" className="flex-row flex-row--gap-sm">
                                 {
-                                    anime?.franchise.socials?.website &&
+                                    anime.franchise.socials.website &&
                                     <SocialMediaCard 
                                         type="website"
-                                        social={anime?.franchise.socials.website}
+                                        social={anime.franchise.socials.website}
                                     />
                                 }
                                 {
-                                    anime?.franchise.socials?.youtube &&
+                                    anime.franchise.socials.youtube &&
                                     <SocialMediaCard 
                                         type="youtube"
-                                        social={anime?.franchise.socials.youtube}
+                                        social={anime.franchise.socials.youtube}
                                     />
                                 }
                                 {
-                                    anime?.franchise.socials?.reddit &&
+                                    anime.franchise.socials.reddit &&
                                     <SocialMediaCard 
                                         type="reddit"
-                                        social={anime?.franchise.socials.reddit}
+                                        social={anime.franchise.socials.reddit}
                                     />
                                 }
                                 {
-                                    anime?.franchise.socials?.twitter &&
+                                    anime.franchise.socials.twitter &&
                                     <SocialMediaCard 
                                         type="twitter"
-                                        social={anime?.franchise.socials.twitter}
+                                        social={anime.franchise.socials.twitter}
                                     />
                                 }
                             </div>
@@ -150,12 +140,12 @@ export default function AnimeDetails() {
                         <div id="misc">
                             <ArcHeader title="Misc" />
                             <div className="flex-row flex-row--gap-sm">
-                                <InfoItem label="Season" value={anime?.season} />
-                                <InfoItem label="Type" value={anime?.type} />
-                                <InfoItem label="Status" value={anime?.status} />
-                                <InfoItem label="Start Date" value={anime?.airingStartDate} />
-                                <InfoItem label="End Date" value={anime?.airingEndDate} />
-                                <InfoItem label="Studio" value={anime?.studio?.name} />
+                                <InfoItem label="Season" value={anime.season} />
+                                <InfoItem label="Type" value={anime.type} />
+                                <InfoItem label="Status" value={anime.status} />
+                                <InfoItem label="Start Date" value={anime.airingStartDate} />
+                                <InfoItem label="End Date" value={anime.airingEndDate} />
+                                <InfoItem label="Studio" value={anime.studio.name} />
 
                             </div>
                         </div>
@@ -165,7 +155,7 @@ export default function AnimeDetails() {
                             <ArcHeader title="Characters" />
                             <div id="characters-container" className="flex-col flex-col--gap-sm">
                                 {
-                                    anime?.characters.map((character: any, idx: number) => (
+                                    anime.characters.slice(0,6).map((character: any, idx: number) => (
                                         <CharacterCard 
                                             key={idx}
                                             characterId={character.character.id}
@@ -183,23 +173,23 @@ export default function AnimeDetails() {
                             <ArcHeader title="Anime Flow" />
                             <div className="grid grid--2-col">
                                 {
-                                    anime?.previousAnime ?
+                                    anime.previousAnime ?
                                         <MediaFlowCard 
-                                            image={`/storage/miru/${anime?.previousAnime.fromAnime.id}.jpg`}
+                                            image={`/storage/miru/${anime.previousAnime.fromAnime.id}.jpg`}
                                             relation="Prequel"
-                                            mediaName={anime ? anime?.previousAnime.fromAnime.title : 'Loading'}
-                                            mediaLink={`/miru/anime/${anime?.previousAnime.fromAnime.id}/${anime?.previousAnime.fromAnime.slug}`}              
+                                            mediaName={anime ? anime.previousAnime.fromAnime.title : 'Loading'}
+                                            mediaLink={`/miru/anime/${anime.previousAnime.fromAnime.id}/${anime?.previousAnime.fromAnime.slug}`}              
                                         />
                                     :
                                         <p>No Previous Anime</p>
                                 }
                                 {
-                                    anime?.nextAnime ?
+                                    anime.nextAnime ?
                                         <MediaFlowCard 
-                                            image={`/storage/miru/${anime?.nextAnime.toAnime.id}.jpg`}
+                                            image={`/storage/miru/${anime.nextAnime.toAnime.id}.jpg`}
                                             relation="Prequel"
-                                            mediaName={anime ? anime?.nextAnime.toAnime.title : 'Loading'}
-                                            mediaLink={`/miru/anime/${anime?.nextAnime.toAnime.id}/${anime?.nextAnime.toAnime.slug}`}              
+                                            mediaName={anime ? anime.nextAnime.toAnime.title : 'Loading'}
+                                            mediaLink={`/miru/anime/${anime.nextAnime.toAnime.id}/${anime.nextAnime.toAnime.slug}`}              
                                         />
                                     :
                                         <p>No Previous Anime</p>
@@ -212,7 +202,7 @@ export default function AnimeDetails() {
                                     <ArcHeader title="Openings" />
                                     <div className="flex-row flex-row--gap-sm row-divider">
                                         {
-                                            anime?.themes.opening.length ?
+                                            anime.themes.opening.length ?
                                                 anime.themes.opening.map((op: AnimeTheme, idx: number) => (
                                                 <KikuCard 
                                                     key={idx}
@@ -232,7 +222,7 @@ export default function AnimeDetails() {
                                     <ArcHeader title="Endings" />
                                     <div className="flex-row flex-row--gap-sm row-divider">
                                         {
-                                            anime?.themes.ending?.length ?
+                                            anime.themes.ending.length ?
                                                 anime.themes.ending.map((ed: AnimeTheme, idx: number) => (
                                                     <KikuCard 
                                                         key={idx}
